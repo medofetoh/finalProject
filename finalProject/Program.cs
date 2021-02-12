40bab9085e6c4055a671c60ebd26fa6c3a919104
medo <engmedofetoh2016@gmail.com>
2021-02-12 00:48:49 -0500

    Add project files.

---
+++ finalProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace finalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var proj1 = new Task1();
+           // proj1.DoAction();
+            //var proj2 = new Task2();
+            //proj2.DoAction();
+            var proj3 = new Task3();
+            proj3.DoAction();
+        }
+    }
+}
---
+++ finalProject/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace finalProject
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ClassName { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public string LastClassCompleted { get; set; }
+
+        public DateTimeOffset LastClassCompletedOn { get; set; }
+
+
+    }
+}
