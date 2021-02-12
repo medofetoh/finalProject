c4bbe00df829697f08f20ae2d093232b93617043
medo <engmedofetoh2016@gmail.com>
2021-02-12 01:00:24 -0500

    add menu list in the main method

--- finalProject/Program.cs
+++ finalProject/Program.cs
@@ -6,12 +6,24 @@ namespace finalProject
     {
         static void Main(string[] args)
         {
-           // var proj1 = new Task1();
-           // proj1.DoAction();
-            //var proj2 = new Task2();
-            //proj2.DoAction();
+            Console.WriteLine("To try simple level enter 1 ,to try lists enter 2 , to try work with json enter 3");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 1) {
+                var proj1 = new Task1();
+                proj1.DoAction();
+            }
+             else if (num == 2)
+            {
+
+                var proj2 = new Task2();
+                proj2.DoAction();
+            }
+
+            else { 
             var proj3 = new Task3();
             proj3.DoAction();
+        }
         }
     }
 }
